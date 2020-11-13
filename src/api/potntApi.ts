5b312817f13915b630d633b5bbb4bfde288a252c
lucaschimweg <luca@schimweg.net>
2020-11-13 18:59:33 +0100

    Add API

---
+++ src/api/potntApi.ts
@@ -0,0 +1,114 @@
+import RestfulApi from "./restfulApi";
+
+export type Pothole = {
+    uuid: string,
+    length: number,
+    width: number,
+    depth: number,
+    coordinate: Coordinate,
+    road: Road,
+}
+
+export type Road = {
+    uuid: string,
+    name: string
+}
+
+export type Coordinate = {
+    latitude: number,
+    longitude: number
+}
+
+export class PotntSignupApi extends RestfulApi {
+    constructor() {
+        super("/api");
+    }
+
+    async signUp(username: string, password: string, name: string): Promise<PotntApi | undefined> {
+        let res = await this.post("/signup", {
+            username: username,
+            password: password,
+            name: name
+        });
+
+        if (!(res.status == 200 && res.resultParsable)) {
+            return undefined
+        }
+
+        let api = new PotntApi(name);
+        api.bearerToken = res.result;
+        return api
+    }
+}
+
+export class PotntApi extends RestfulApi {
+
+    constructor(tenant: string) {
+        super(`/api/${tenant}`);
+    }
+
+    async login(username: string, password: string): Promise<boolean> {
+        let res = await this.post("/login", {
+            "username": username,
+            "password": password
+        });
+
+        if (!(res.status == 200 && res.resultParsable)) {
+            return false
+        }
+
+        this.bearerToken = res.result
+        return true
+    }
+
+    async getPothole(uuid: string): Promise<Pothole | undefined> {
+        let res = await this.get(`/pothole/${uuid}`);
+        if (!(res.status == 200 && res.resultParsable)) {
+            return undefined
+        }
+
+        return res.result
+    }
+
+    async getRoads(): Promise<Road[] | undefined> {
+        let res = await this.get(`/roads`);
+        if (!(res.status == 200 && res.resultParsable)) {
+            return undefined
+        }
+
+        return res.result
+    }
+
+    async getPotholesOnRoad(uuid: string): Promise<Pothole[] | undefined> {
+        let res = await this.get(`/roads/${uuid}/potholes`);
+        if (!(res.status == 200 && res.resultParsable)) {
+            return undefined
+        }
+
+        return res.result
+    }
+
+    async deletePothole(uuid: string): Promise<boolean> {
+        let res = await this.delete(`/potholes/${uuid}`);
+        return (res.status == 200 && res.resultParsable);
+    }
+
+    async updatePothole(pothole: Pothole): Promise<Pothole | undefined> {
+        let res = await this.put(`/pothole/${pothole.uuid}`, pothole);
+        if (!(res.status == 200 && res.resultParsable)) {
+            return undefined
+        }
+
+        return res.result
+    }
+
+    async addPothole(pothole: Pothole): Promise<Pothole | undefined> {
+        delete pothole.uuid
+        let res = await this.post(`/pothole`, pothole);
+        if (!(res.status == 200 && res.resultParsable)) {
+            return undefined
+        }
+
+        return res.result
+    }
+}
